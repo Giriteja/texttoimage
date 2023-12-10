aebbda8e802dedc015d130a8b357b8c4f38a7446
Giriteja <40452942+Giriteja@users.noreply.github.com>
2023-12-10 22:01:21 +0530

    Update dall3.py

--- dall3.py
+++ dall3.py
@@ -20,6 +20,28 @@ import webbrowser
 import os
 
 #export GOOGLE_APPLICATION_CREDENTIALS="learninpad-4341edd012dc.json"
+from google.oauth2 import service_account
+from google.cloud import storage
+import os
+
+# Function to create GCP credentials from environment variables
+def create_gcp_credentials():
+    credentials = service_account.Credentials.from_service_account_info({
+        "type": "service_account",
+        "project_id": os.getenv("GCP_PROJECT_ID"),
+        "private_key_id": os.getenv("GCP_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("GCP_PRIVATE_KEY").replace('\\n', '\n'),
+        "client_email": os.getenv("GCP_CLIENT_EMAIL"),
+        "client_id": os.getenv("GCP_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("GCP_CLIENT_X509_CERT_URL")
+    })
+    return credentials
+
+# Use the custom credentials when initializing the storage client
+storage_client = storage.Client(credentials=create_gcp_credentials())
 
 
 # Replace YOUR_API_KEY with your OpenAI API key
@@ -41,7 +63,7 @@ import urllib.request
 
 from google.cloud import storage
 
-storage_client = storage.Client()
+#storage_client = storage.Client()
 
 st.title("Story Illustriator")
 
