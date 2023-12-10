3955180ffdaefd745acd605046a06e6e112b3fdc
Giriteja <40452942+Giriteja@users.noreply.github.com>
2023-12-10 22:39:36 +0530

    Update dall3.py

--- dall3.py
+++ dall3.py
@@ -28,15 +28,15 @@ import os
 def create_gcp_credentials():
     credentials = service_account.Credentials.from_service_account_info({
         "type": "service_account",
-        "project_id": os.getenv("GCP_PROJECT_ID"),
-        "private_key_id": os.getenv("GCP_PRIVATE_KEY_ID"),
-        "private_key": os.getenv("GCP_PRIVATE_KEY").replace('\\n', '\n'),
-        "client_email": os.getenv("GCP_CLIENT_EMAIL"),
-        "client_id": os.getenv("GCP_CLIENT_ID"),
+        "project_id": os.getenv("project_id"),
+        "private_key_id": os.getenv("private_key_id"),
+        "private_key": os.getenv("private_key").replace('\\n', '\n'),
+        "client_email": os.getenv("client_email"),
+        "client_id": os.getenv("client_id"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.getenv("GCP_CLIENT_X509_CERT_URL")
+        "client_x509_cert_url": os.getenv("client_x509_cert_url")
     })
     return credentials
 
