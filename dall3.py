037576af6d0ee0c57ce45d2000ac1af90264c2da
Giriteja <40452942+Giriteja@users.noreply.github.com>
2023-12-10 23:07:06 +0530

    Update dall3.py

--- dall3.py
+++ dall3.py
@@ -35,8 +35,9 @@ def create_gcp_credentials():
         "client_id": os.getenv("client_id"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": ""https://www.googleapis.com/oauth2/v1/certs"",
-        "client_x509_cert_url": os.getenv("client_x509_cert_url")
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.getenv("client_x509_cert_url"),
+	"universe_domain":"googleapis.com"
     })
     return credentials
 
