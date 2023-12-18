0470b5b05651b12109bb81780458ef61da4dd6b2
Giriteja <40452942+Giriteja@users.noreply.github.com>
2023-12-18 19:51:09 +0530

    prompt change

--- dall3.py
+++ dall3.py
@@ -100,7 +100,7 @@ def fetch_imagedescription_and_script(prompt,url,headers):
 
     # Define the payload for the chat model
     messages = [
-        {"role": "system", "content": "You are an expert In dividing paragrpah into subparagraphs and Generating Image Descriptions related to dalle for each Subparagraph."},
+        {"role": "system", "content": "You are an expert In dividing paragrpah into subparagraphs and Generating Image Descriptions related to dalle for each Subparagraph.don't generate extra text stick to given paragraph"},
         {"role": "user", "content": prompt}
     ]
 
