b2b3ae0be767ac078594d4f3544a4ae4e6328172
Giriteja <40452942+Giriteja@users.noreply.github.com>
2024-03-19 22:30:11 +0530

    Update dall3.py

--- dall3.py
+++ dall3.py
@@ -374,5 +374,6 @@ with tab3:
 	    # List blobs in the specified GCS bucket folder
 	    if(class_name1 and subject_name1 and lesson_name123):
 	        blobs = storage_client.list_blobs(bucket_name, prefix=folder_name)
-	        st.write(blobs)
+	        for blob in blobs:
+			st.write(blob)
 	            
